e0d86c0f728e0c475570f3d272d097e3bfa8fa9e
masterdpro <youri.d2.bel@gmail.com>
2023-08-12 15:13:17 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+let count = 0;
+
+app.get('/api', (req, res) => {
+res.json({count})
+})
+
+app.post('/api', (req, res) => {
+++count;
+res.json({count});
+});
+
+app.listen(port, () => {
+console.log(`Example app listening on port ${port}`)
+})
